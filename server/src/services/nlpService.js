8c586a431d3e86efaa43c7bdf6eb867da49b03af
ProjektKevin <minthetkhine05@gmail.com>
2025-11-21 15:06:22 +0800

    feat(): created food list and nlp

---
+++ server/src/services/nlpService.js
@@ -0,0 +1,27 @@
+// Function to clean the text (remove white spaces and what not)
+const cleanText = (text) => {
+  return text
+    .toLowerCase()
+    .replace(/[^\w\s]/g, "")
+    .replace(/\s+/g, " ")
+    .trim();
+}
+
+
+// Classical NLP to detect food from predermined food list
+const detectFoods = (transcript, foodList) => {
+  const text = cleanText(transcript);
+
+  const detected = [];
+
+  for (const food of foodList) {
+    const f = food.toLowerCase();
+    if (text.includes(f)) {
+      detected.push(food);
+    }
+  }
+
+  return detected;
+}
+
+export { detectFoods };
